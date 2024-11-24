0fc0ce9f4d6823200f5da8dab200b1b07d0ad1e3
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2024-11-24 15:33:56 +0100

    fd

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import './index.css'
 // Ensure the selector matches the ID of your root element in index.html
 const rootElement = document.getElementById('root');
 import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import { SpeedInsights } from '@vercel/speed-insights/react';
+
 import ReactGa from 'react-ga'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 ReactGa.initialize('G-7VD9KSTCK9')
